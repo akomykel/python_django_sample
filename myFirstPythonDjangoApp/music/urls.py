cee2640ac164a20a117d5ccfabae6fa828d24cc5
akomykel <akomykel@gmail.com>
2019-01-04 15:04:33 +0800

    Mykel: done until tutorial 35

--- myFirstPythonDjangoApp/music/urls.py
+++ myFirstPythonDjangoApp/music/urls.py
@@ -4,12 +4,15 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-    # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
-    # /music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
 
-    # /music/<album_id>/favorite/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 ]
